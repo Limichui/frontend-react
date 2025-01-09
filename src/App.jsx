96e837972235c6c481d05fc19b99284b8e1c6399
Limichui <limbert.olmos.m@gmail.com>
2025-01-08 21:12:04 -0400

    Agregar screens/Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import './App.css'
-import Logo from './components/home/Logo.jsx'
-import Body from './components/home/Body.jsx'
-import Footer from './components/home/Footer.jsx'
-
+import Home  from './screens/Home.jsx';
 
 function App() {
   return (
     <>
-      <Logo></Logo>
-      <Body></Body>
-      <Footer></Footer>
+        <Home />
     </>
   )
 }
 
-export default App
+export default App;
