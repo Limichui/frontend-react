b5fee225801d59faeecb11c3785b5af4f709ac64
Limichui <limbert.olmos.m@gmail.com>
2025-01-09 21:36:17 -0400

    Laboratorio 6

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -3,13 +3,17 @@ import OpenLink from '../components/core/OpenLink.jsx';
 
 import Body from '../components/Home/Body.jsx';
 import Footer from '../components/Home/Footer.jsx';
+import { useSelector, useDispatch } from 'react-redux'
+import { decrement, increment } from '../store/features/counter/counterSlice.js'
 
 import viteLogo from "../assets/vite.svg"
 import reactLogo from "../assets/react.svg"
 import githubLogo from "../assets/github.svg"
 const Home = () => {
+    const count = useSelector((state) => state.counter.value)
     return (
         <div>
+            <span>{count}</span>
             <div>
                 <OpenLink
                     redirectURL = "https://vite.dev"
